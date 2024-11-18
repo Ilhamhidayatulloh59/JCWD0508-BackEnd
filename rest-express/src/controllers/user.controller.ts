c848668ab5217ba356e8715c1c8a5797457d9e04
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2024-11-18 16:21:23 +0700

    rest api

--- rest-express/src/controllers/user.controller.ts
+++ rest-express/src/controllers/user.controller.ts
@@ -4,9 +4,17 @@ import { IUser } from "../types/user";
 
 export class UserController {
   getUsers(req: Request, res: Response) {
-    const users: IUser[] = JSON.parse(
+    const { name } = req.query;
+    let users: IUser[] = JSON.parse(
       fs.readFileSync("./db/users.json", "utf-8")
     );
+
+    if (name) {
+      users = users.filter((item) =>
+        item.name.toLowerCase().includes(name as string)
+      );
+    }
+
     res.status(200).send({ users });
   }
   getUserId(req: Request, res: Response) {
@@ -15,23 +23,43 @@ export class UserController {
       fs.readFileSync("./db/users.json", "utf-8")
     );
     const data = users.find((item) => item.id == +id);
-    if (data) {
-      res.status(200).send({ user: data });
-    } else {
-      res.status(400).send({ message: "User not found!" });
-    }
+    res.status(200).send({ user: data });
   }
   addUser(req: Request, res: Response) {
     const users: IUser[] = JSON.parse(
       fs.readFileSync("./db/users.json", "utf-8")
     );
-    const id = Math.max(...users.map((item) => item.id)) + 1;
+    const maxId = Math.max(...users.map((item) => item.id));
+    const id = users.length == 0 ? 1 : maxId + 1;
     const { name, email, password } = req.body;
     const newData: IUser = { id, name, email, password };
     users.push(newData);
 
     fs.writeFileSync("./db/users.json", JSON.stringify(users), "utf-8");
 
-    res.status(200).send({ user: newData });
+    res.status(201).send({ user: newData });
+  }
+  editUser(req: Request, res: Response) {
+    const { id } = req.params;
+    const users: IUser[] = JSON.parse(
+      fs.readFileSync("./db/users.json", "utf-8")
+    );
+    const idx: number = users.findIndex((item) => item.id == +id);
+    users[idx] = { ...users[idx], ...req.body };
+
+    fs.writeFileSync("./db/users.json", JSON.stringify(users), "utf-8");
+
+    res.status(200).send("edit succesfully!");
+  }
+  deleteUser(req: Request, res: Response) {
+    const { id } = req.params;
+    const users: IUser[] = JSON.parse(
+      fs.readFileSync("./db/users.json", "utf-8")
+    );
+    const newUsers = users.filter((item) => item.id != +id);
+
+    fs.writeFileSync("./db/users.json", JSON.stringify(newUsers), "utf-8");
+
+    res.status(200).send("Delete Successfully!");
   }
 }
