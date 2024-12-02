39d12b030acc746ca0b8cd90ec324aea2d066a09
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2024-12-02 16:06:36 +0700

    add auth

--- express-prisma/src/controllers/auth.controller.ts
+++ express-prisma/src/controllers/auth.controller.ts
@@ -8,10 +8,10 @@ export class AuthController {
   async registerUser(req: Request, res: Response) {
     try {
       const { password, confirmPassword, username, email } = req.body;
-      if (password != confirmPassword) throw "Password not match!";
+      if (password != confirmPassword) throw { message: "Password not match!" };
 
       const user = await findUser(username, email);
-      if (user) throw "username or email has been used !";
+      if (user) throw { message: "username or email has been used !" };
 
       const salt = await genSalt(10);
       const hashPasword = await hash(password, salt);
@@ -19,7 +19,7 @@ export class AuthController {
       await prisma.user.create({
         data: { username, email, password: hashPasword },
       });
-      res.status(201).send("Register Successfully ✅");
+      res.status(201).send({ message: "Register Successfully ✅" });
     } catch (err) {
       console.log(err);
       res.status(400).send(err);
@@ -30,19 +30,44 @@ export class AuthController {
       const { data, password } = req.body;
       const user = await findUser(data, data);
 
-      if (!user) throw "Account not found !";
+      if (!user) throw { message: "Account not found !" };
+      if (user.isSuspend) throw { message: "Account Suspended !" };
 
       const isValidPass = await compare(password, user.password);
-      if (!isValidPass) throw "Incorrect Password !";
+      if (!isValidPass) {
+        await prisma.user.update({
+          data: { loginAttempt: { increment: 1 } },
+          where: { id: user.id },
+        });
+        if (user.loginAttempt == 2) {
+          await prisma.user.update({
+            data: { isSuspend: true },
+            where: { id: user.id },
+          });
+        }
+        throw { message: "Incorrect Password !" };
+      }
+
+      await prisma.user.update({
+        data: { loginAttempt: 0 },
+        where: { id: user.id },
+      });
 
       const payload = { id: user.id, role: user.role };
-      const token = sign(payload, process.env.JWT_KEY!, { expiresIn: "10m" });
+      const token = sign(payload, process.env.JWT_KEY!, { expiresIn: "1d" });
 
-      res.status(200).send({
-        message: "Login Sucessfully ✅",
-        token,
-        user,
-      });
+      res
+        .status(200)
+        .cookie("token", token, {
+          httpOnly: true,
+          maxAge: 24 * 3600 * 1000,
+          path: "/",
+          secure: process.env.NODE_ENV === "production",
+        })
+        .send({
+          message: "Login Sucessfully ✅",
+          user,
+        });
     } catch (err) {
       console.log(err);
       res.status(400).send(err);
