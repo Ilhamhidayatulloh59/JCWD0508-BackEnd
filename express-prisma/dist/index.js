31e5cbd7e206c55cff9e3755996f763b9299596a
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2024-12-13 13:59:27 +0700

    change base url

--- express-prisma/dist/index.js
+++ express-prisma/dist/index.js
@@ -15,7 +15,7 @@ const PORT = 8000;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
-    origin: "http://localhost:3000",
+    origin: process.env.BASE_URL_FE,
     credentials: true,
 }));
 app.use((0, cookie_parser_1.default)());
