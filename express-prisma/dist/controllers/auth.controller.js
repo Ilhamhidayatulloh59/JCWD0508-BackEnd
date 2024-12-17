fe87436afb2c1f45165039453f76d8bea0ba8b6d
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2024-12-17 16:14:18 +0700

    edit domain

--- express-prisma/dist/controllers/auth.controller.js
+++ express-prisma/dist/controllers/auth.controller.js
@@ -97,7 +97,7 @@ class AuthController {
                     sameSite: "none",
                     maxAge: 24 * 3600 * 1000,
                     path: "/",
-                    domain: "blogger-fe.vercel.app/",
+                    domain: "blogger-fe.vercel.app",
                 })
                     .send({
                     message: "Login Sucessfully ✅",
